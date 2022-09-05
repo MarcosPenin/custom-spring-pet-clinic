b6a0cfebfe967cda50306a9229b1774f31834c6f
Marcos Penin <marcosgonzalezpenin@gmail.com>
2022-09-05 15:54:16 +0200

    Innitial Project add

---
+++ src/main/java/guru/springframework/customspringpetclinic/CustomSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.customspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/customspringpetclinic/CustomSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.customspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
