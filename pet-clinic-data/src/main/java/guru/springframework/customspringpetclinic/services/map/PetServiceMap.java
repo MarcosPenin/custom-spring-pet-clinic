d471d45e358dac592d95ad2df61e87a77cdfbbb6
Marcos Penin <marcosgonzalezpenin@gmail.com>
2022-09-06 17:06:40 +0200

    adding bootstrap

--- pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package guru.springframework.customspringpetclinic.services.map;
 
 import guru.springframework.customspringpetclinic.model.Pet;
 import guru.springframework.customspringpetclinic.services.CrudService;
+import guru.springframework.customspringpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package guru.springframework.customspringpetclinic.services.map;
 
 import guru.springframework.customspringpetclinic.model.Vet;
 import guru.springframework.customspringpetclinic.services.CrudService;
+import guru.springframework.customspringpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
