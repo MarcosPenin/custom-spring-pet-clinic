7186af60be8fa943334195383ebaaaaf6d033522
Marcos Penin <marcosgonzalezpenin@gmail.com>
2022-09-08 15:41:01 +0200

    setting JPA entities

--- pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/customspringpetclinic/model/Pet.java
@@ -1,13 +1,26 @@
 package guru.springframework.customspringpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
-
+    @Column(name = "name")
     private String name;
-    private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
 
     public String getName() {
         return name;
@@ -39,5 +52,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
